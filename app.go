cacd743b4b93e6ba28a2c4ddcb2a7303417ae2eb
KhanhhNe <khanhluong3005@gmail.com>
2025-11-12 16:59:11 +0700

    Add lock for 1 instance only

--- app.go
+++ app.go
@@ -3,10 +3,6 @@ package main
 import (
 	"context"
 	"fmt"
-	"go-proxy/common"
-	"go-proxy/proxyserver"
-	"sync"
-	"time"
 )
 
 // App struct
@@ -17,63 +13,6 @@ type App struct {
 // NewApp creates a new App application struct
 func NewApp() *App {
 	return &App{}
-}
-
-// startup is called when the app starts. The context is saved
-// so we can call the runtime methods
-func (a *App) startup(ctx context.Context) {
-	a.ctx = ctx
-
-	ListenerServerManager.AddListeners([]*LocalListener{
-		NewLocalListener(8000, &common.ProxyAuth{
-			Username: "khanh",
-			Password: "khanh",
-		}, ServerFilter{Tags: []string{"ssh"}}),
-		NewLocalListener(8001, &common.ProxyAuth{
-			Username: "khanh",
-			Password: "khanh",
-		}, ServerFilter{Tags: []string{"http"}}),
-		NewLocalListener(8002, &common.ProxyAuth{
-			Username: "khanh",
-			Password: "khanh",
-		}, ServerFilter{Tags: []string{"socks5"}}),
-
-		NewLocalListener(9001, &common.ProxyAuth{
-			Username: "khanh",
-			Password: "khanh",
-		}, ServerFilter{IgnoreAll: true}),
-		NewLocalListener(9002, &common.ProxyAuth{
-			Username: "khanh",
-			Password: "khanh",
-		}, ServerFilter{IgnoreAll: true}),
-	})
-
-	var wg sync.WaitGroup
-	wg.Go(func() {
-		ListenerServerManager.Serve()
-	})
-	<-time.After(time.Second)
-
-	ListenerServerManager.AddServers([]*proxyserver.Server{
-		proxyserver.NewServer("localhost", 2222, &common.ProxyAuth{
-			Username: "ubuntu",
-			Password: "ubuntu",
-		}),
-		proxyserver.NewServer("127.0.0.1", 9001, &common.ProxyAuth{
-			Username: "khanh",
-			Password: "khanh",
-		}),
-		proxyserver.NewServer("::1", 9002, &common.ProxyAuth{
-			Username: "khanh",
-			Password: "khanh",
-		}),
-	})
-
-	for _, s := range ListenerServerManager.Servers {
-		fmt.Println(s)
-	}
-
-	wg.Wait()
 }
 
 // Greet returns a greeting for the given name
--- main.go
+++ main.go
@@ -4,14 +4,22 @@ import (
 	"context"
 	"embed"
 	"fmt"
+	"go-proxy/common"
+	"go-proxy/proxyserver"
+	"strings"
+	"sync"
+	"time"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
 //go:embed all:frontend/dist
 var assets embed.FS
+
+var wailsContext *context.Context
 
 func main() {
 	fmt.Println("Hey skipped Wails app. Remember to turn it back on")
@@ -31,7 +39,11 @@ func main() {
 		},
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
 		OnStartup:        app.startup,
-		Bind: []interface{}{
+		SingleInstanceLock: &options.SingleInstanceLock{
+			UniqueId:               "goproxy-db2d80a7-7116-4ed5-a61c-b641cf3d1b8c",
+			OnSecondInstanceLaunch: app.onSecondInstanceLaunch,
+		},
+		Bind: []any{
 			app,
 		},
 	})
@@ -39,4 +51,72 @@ func main() {
 	if err != nil {
 		println("Error:", err.Error())
 	}
+}
+
+// startup is called when the app starts. The context is saved
+// so we can call the runtime methods
+func (a *App) startup(ctx context.Context) {
+	wailsContext = &ctx
+	a.ctx = ctx
+
+	ListenerServerManager.AddListeners([]*LocalListener{
+		NewLocalListener(8000, &common.ProxyAuth{
+			Username: "khanh",
+			Password: "khanh",
+		}, ServerFilter{Tags: []string{"ssh"}}),
+		NewLocalListener(8001, &common.ProxyAuth{
+			Username: "khanh",
+			Password: "khanh",
+		}, ServerFilter{Tags: []string{"http"}}),
+		NewLocalListener(8002, &common.ProxyAuth{
+			Username: "khanh",
+			Password: "khanh",
+		}, ServerFilter{Tags: []string{"socks5"}}),
+
+		NewLocalListener(9001, &common.ProxyAuth{
+			Username: "khanh",
+			Password: "khanh",
+		}, ServerFilter{IgnoreAll: true}),
+		NewLocalListener(9002, &common.ProxyAuth{
+			Username: "khanh",
+			Password: "khanh",
+		}, ServerFilter{IgnoreAll: true}),
+	})
+
+	var wg sync.WaitGroup
+	wg.Go(func() {
+		ListenerServerManager.Serve()
+	})
+	<-time.After(time.Second)
+
+	ListenerServerManager.AddServers([]*proxyserver.Server{
+		proxyserver.NewServer("localhost", 2222, &common.ProxyAuth{
+			Username: "ubuntu",
+			Password: "ubuntu",
+		}),
+		proxyserver.NewServer("127.0.0.1", 9001, &common.ProxyAuth{
+			Username: "khanh",
+			Password: "khanh",
+		}),
+		proxyserver.NewServer("::1", 9002, &common.ProxyAuth{
+			Username: "khanh",
+			Password: "khanh",
+		}),
+	})
+
+	for _, s := range ListenerServerManager.Servers {
+		fmt.Println(s)
+	}
+
+	wg.Wait()
+}
+
+func (a *App) onSecondInstanceLaunch(secondInstanceData options.SecondInstanceData) {
+	secondInstanceArgs := secondInstanceData.Args
+
+	println("user opened second instance", strings.Join(secondInstanceData.Args, ","))
+	println("user opened second from", secondInstanceData.WorkingDirectory)
+	runtime.WindowUnminimise(*wailsContext)
+	runtime.Show(*wailsContext)
+	go runtime.EventsEmit(*wailsContext, "launchArgs", secondInstanceArgs)
 }
